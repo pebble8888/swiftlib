a2072d97be1485d556d1c2db963ce5229c10c760
pebble8888 <pebble8888@gmail.com>
2015-09-26 14:11:05 +0900

    add framework

---
+++ swiftlib/swiftlib/swiftlib.h
@@ -0,0 +1,19 @@
+//
+//  swiftlib.h
+//  swiftlib
+//
+//  Created by pebble8888 on 2015/09/26.
+//  Copyright (c) 2015年 pebble8888. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftlib.
+FOUNDATION_EXPORT double swiftlibVersionNumber;
+
+//! Project version string for swiftlib.
+FOUNDATION_EXPORT const unsigned char swiftlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftlib/PublicHeader.h>
+
+
